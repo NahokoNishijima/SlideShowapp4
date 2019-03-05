dc44b958c0a1b515d279923deaf79fc9217785bd
NahokoNishijima <naho_kokko@yahoo.co.jp>
2019-03-05 10:15:36 +0900

    Initial Commit

---
+++ SlideShowApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SlideShowApp
+//
+//  Created by 西島菜穂子 on 2019/03/05.
+//  Copyright © 2019 nahoko.nishijima. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
